6e4ad0a680a3922e2a78dcf4eb03e2ded5e2ce0d
Jan Nicklas <j.nicklas@me.com>
2024-11-12 12:18:22 +0100

    turbopack

--- @segments/carousel/src/buttons.ts
+++
@@ -1,9 +0,0 @@
-import styles from './button.module.css';
-
-export const CarouselButton = ({
-  className = '',
-}) => {
-  return `<button class="${styles.button + (
-    className ? ` ${className}` : ''
-  )}">Carousel Button</button>`;
-};
